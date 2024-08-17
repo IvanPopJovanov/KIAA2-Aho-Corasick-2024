a562d7f4d6d1bba3c87be4e4058be8260db38a1b
Ivan Pop-Jovanov <ivanpopjovanov@yahoo.com>
2024-08-17 19:05:02 +0200

    add match method

--- PatternMatchingMachine.cpp
+++ PatternMatchingMachine.cpp
@@ -1,1 +1,20 @@
 #include "PatternMatchingMachine.h"
+#include <iostream>
+
+void PatternMatchingMachine::match()
+{
+    State *state = &(states[0]);
+    int n = K.size();
+    for(int i=0; i<n; ++i) {
+        while(g(state, x[i]) == FAIL)
+            state = f(state);
+        state = g(state, x[i]);
+        if(output(state).size() > 0) {
+            std::cout << i << ": ";
+            for (auto x : output(state)) {
+                std::cout << x << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+}
--- PatternMatchingMachine.h
+++ PatternMatchingMachine.h
@@ -1,7 +1,17 @@
 #pragma once
+#include "State.h"
 #include <string>
+#include <vector>
 
 struct PatternMatchingMachine {
   const std::string & x;
-  PatternMatchingMachine(const std::string & x) : x(x) {};
+  const std::vector<std::string> & K;
+  PatternMatchingMachine(const std::string & x, const std::vector<std::string> & K) : x(x), K(K) {};
+  std::vector<State> states;
+
+  State* g(const State * state, const char & ch);
+  State* f(const State * state);
+  std::vector<std::string> output(const State * state);
+
+  void match();
 };
