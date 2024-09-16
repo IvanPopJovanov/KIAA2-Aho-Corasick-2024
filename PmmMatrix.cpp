5b4e1c4482338baf84a118a58a63f4a751752a93
Ivan Pop-Jovanov <ivanpopjovanov@yahoo.com>
2024-09-16 15:39:18 +0200

    PmmMatrix definicije funkcija prebacene iz header fajla u cpp fajl

--- PmmMatrix.cpp
+++ PmmMatrix.cpp
@@ -1,1 +1,83 @@
 #include "PmmMatrix.h"
+
+void PmmMatrix::enter(const std::string &a, int &newstate)
+{
+  int m = a.size();
+  state = 0;
+  int j = 0;
+  while(g[state][a[j]] != FAIL) {
+    state = g[state][a[j]];
+    ++j;
+  }
+  for(int p = j; p < m; ++p) {
+    ++newstate;
+    g[state][a[p]] = newstate;
+    state = newstate;
+  }
+  output[state] = true;
+  outputs[state].push_back(a);
+}
+
+void PmmMatrix::construct_g()
+{
+  int newstate = 0;
+  for(int i = 0; i < k; ++i) {
+    enter(K[i], newstate);
+  }
+  for(int a = 0; a < ALPHABET_SIZE; ++a) {
+    if(g[0][a] == FAIL)
+      g[0][a] = 0;
+  }
+}
+
+void PmmMatrix::construct_f()
+{
+  std::queue<int> queue;
+
+  for(int a = 0; a < ALPHABET_SIZE; ++a) {
+    int s = g[0][a];
+    if(s != 0) {
+      queue.push(s);
+      f[s] = 0;
+    }
+  }
+  
+  while(!queue.empty()) {
+    int r = queue.front();
+    queue.pop(); 
+
+    for(int a = 0; a < ALPHABET_SIZE; ++a) {
+      int s = g[r][a];
+      if(s != FAIL) {
+        queue.push(s);
+        state = f[r];
+        while (g[state][a] == FAIL)
+          state = f[state];
+        f[s] = g[state][a];
+        for(auto o : outputs[f[s]]) {
+          output[s] = true;
+          outputs[s].push_back(o);
+        }
+      }
+    }
+  }
+
+}
+
+void PmmMatrix::match()
+{
+  state = 0;
+
+  for(int i = 0; i < n; ++i) {
+    while(g[state][x[i]] == FAIL)
+      state = f[state];
+    state = g[state][x[i]];
+    if(output[state]) {
+      std::cout << i << ": ";
+      for(auto o : outputs[state]) {
+        std::cout << o << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+}
